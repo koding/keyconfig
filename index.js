4eb836c31a4355296304075f58080fc95e240303
tetsuo <ogunduz@gmail.com>
2015-02-27 16:17:16 +0200

    do not expose

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ var _inherits = require('inherits-underscore');
 module.exports = keyconfig;
 _inherits(keyconfig.prototype);
 
-if ('object' === typeof window)
-  window.keyconfig = module.exports;
-
 function keyconfig (obj) {
   if (!(this instanceof keyconfig)) return new keyconfig(obj);
   this.models = _.map(obj, function (keymaps, name) {
